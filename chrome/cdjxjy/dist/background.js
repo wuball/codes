fe6fd82f6629db7c3b39c38a3526c797aa8a6f0f
Clark <10063194@qq.com>
2018-09-01 07:52:18 +0000

    initial

---
+++ chrome/cdjxjy/dist/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(function () {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+          {
+              // That fires when a page's URL contains a 'g' ...
+              conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { urlContains: 'cdjxjy.com' },
+                })
+              ],
+              // And shows the extension's page action.
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+          }
+        ]);
+    });
+});
+
+
+
